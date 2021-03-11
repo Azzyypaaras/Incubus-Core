f2924d6a28798bfa0e043670feb5f2ab57aded39
Dragonoidzero <danielferrerreyes@protonmail.com>
2021-03-10 18:55:34 -0600

    Completed porting all currently relevant pieces of reusable code. Added json gen. Added block entity movement. Added false inventory.

---
+++ src/main/java/azzy/fabric/incubus_core/be/MovementSensitiveBlockEntity.java
@@ -0,0 +1,13 @@
+package azzy.fabric.incubus_core.be;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+
+public interface MovementSensitiveBlockEntity {
+
+    MovementSensitiveBlockEntity[] getObservers();
+
+    void notifyObserver(BlockEntity movedEntity, BlockPos newPos);
+
+    void notifyMoved(BlockPos newPos);
+}

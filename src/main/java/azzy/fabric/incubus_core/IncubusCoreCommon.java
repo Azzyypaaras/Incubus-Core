63af0da6f795110965ec1cd3fbd7b5edb3b09777
Dragonoidzero <danielferrerreyes@protonmail.com>
2021-03-10 18:47:27 -0600

    The march towards a unified library begins. Powered IngredientStack and JsonUtils.

---
+++ src/main/java/azzy/fabric/incubus_core/IncubusCoreCommon.java
@@ -0,0 +1,16 @@
+package azzy.fabric.incubus_core;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class IncubusCoreCommon implements ModInitializer {
+
+	public static final String MODID = "incubus_core";
+	public static final Logger LOG = LogManager.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/azzy/fabric/incubus_core/recipe/MatchingStackAccessor.java
@@ -0,0 +1,8 @@
+package azzy.fabric.incubus_core.recipe;
+
+import net.minecraft.item.ItemStack;
+
+public interface MatchingStackAccessor {
+
+    ItemStack[] getMatchingStacks();
+}

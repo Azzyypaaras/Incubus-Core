76dd11b5b670157a17a614897f10e71b834880a6
Dragonoidzero <danielferrerreyes@protonmail.com>
2021-09-26 20:46:21 -0500

    Fixed some tag errors.

--- src/main/java/net/id/incubus_core/IncubusCore.java
+++ src/main/java/net/id/incubus_core/IncubusCore.java
@@ -2,13 +2,10 @@ package net.id.incubus_core;
 
 import net.id.incubus_core.misc.HandPistonItem;
 import net.id.incubus_core.misc.IncubusToolMaterials;
-import net.id.incubus_core.misc.IncubusItemGroups;
 import net.id.incubus_core.misc.LunarianSaberItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.loader.api.FabricLoader;
-import net.id.incubus_core.systems.DefaultMaterials;
-import net.id.incubus_core.systems.HeatHelper;
 import net.id.incubus_core.systems.RegistryRegistry;
 import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
@@ -17,18 +14,21 @@ import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Random;
+
 
 public class IncubusCore implements ModInitializer {
 
 	public static final String MODID = "incubus_core";
 	public static final Logger LOG = LogManager.getLogger(MODID);
-	public static final Item LUNARIAN_SABER = registerItem("lunarian_saber", new LunarianSaberItem(IncubusToolMaterials.LUNARIAN, 1, 0F, new FabricItemSettings().group(IncubusItemGroups.CORE_GROUP)));
+	public static final Item LUNARIAN_SABER = registerItem("lunarian_saber", new LunarianSaberItem(IncubusToolMaterials.LUNARIAN, 1, 0F, new FabricItemSettings()));
 
 	public static boolean bypassWorthiness;
 
 	@Override
 	public void onInitialize() {
-		if(!FabricLoader.getInstance().isDevelopmentEnvironment())
+		var tempRandom = new Random(System.currentTimeMillis());
+		if(!FabricLoader.getInstance().isDevelopmentEnvironment() && tempRandom.nextInt(100) == 0)
 			LOG.info(IncubusCoreInit.HOLY_CONST);
 
 		genCheck: {
@@ -44,8 +44,8 @@ public class IncubusCore implements ModInitializer {
 
 		RegistryRegistry.init();
 
-		registerItem("hand_piston", new HandPistonItem(new FabricItemSettings().group(IncubusItemGroups.CORE_GROUP).maxCount(1), false));
-		registerItem("hand_piston_advanced", new HandPistonItem(new FabricItemSettings().group(IncubusItemGroups.CORE_GROUP).fireproof().rarity(Rarity.RARE).maxCount(1), true));
+		registerItem("hand_piston", new HandPistonItem(new FabricItemSettings().group(ItemGroup.TOOLS).maxCount(1), false));
+		registerItem("hand_piston_advanced", new HandPistonItem(new FabricItemSettings().group(ItemGroup.TOOLS).fireproof().rarity(Rarity.RARE).maxCount(1), true));
 	}
 
 	public static Item registerItem(String name, Item item) {

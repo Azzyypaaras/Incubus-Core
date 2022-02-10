868aab92cb60f37690a54a9243ac596a1c26a3b9
Jack Papel <jackpapel@live.com>
2022-02-09 21:24:43 -0800

    Document RegistryQueue + lil changes + bump to rc.4
    Added javadocs.
    Changed the LinkedList to a Map.
    Moved onClient(Action) to Action class as action.onClient();
    Added @FunctionalInterface annotation to Action.
    Bumped to rc.4
    
    Sidenotes:
    LinkedHashMap preserves addition order in iteration.
    RegistryQueues may only register one thing per ID, rightfully so.

--- src/main/java/net/id/incubus_core/util/RegistryQueue.java
+++ src/main/java/net/id/incubus_core/util/RegistryQueue.java
@@ -5,53 +5,134 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-import java.util.LinkedList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
+/**
+ * The purpose of this class is to allow you to register
+ * objects while performing actions on them at the same time.
+ * It also allows you to register said objects in one fell swoop
+ * (All at the same time).
+ * <br>Example usage:
+ * <pre>{@code
+ * private static RegistryQueue ITEM = new RegistryQueue(Registry.ITEM);
+ *
+ * private static Action<Item> compostable = (id, item) -> CompostingChanceRegistry.INSTANCE.add(item, 30);
+ *
+ * public static Item CANDY_CANE = ITEM.add("candy_cane", new Item(...), compostable);
+ * public static Item GINGERBREAD_MAN = ITEM.add("ginger_bread_man", new Item(...), compostable);
+ * }</pre>
+ * More complex example usage:
+ * <pre>{@code
+ * private static Action<Item> fuel(int ticks) {
+ *     return (id, item) -> FuelRegistry.INSTANCE.add(item, ticks);
+ * }
+ *
+ * public static Item GIANT_STICK = ITEM.add("giant_stick", new Item(...), fuel(1000));
+ * public static Item STRAW_BOWL = ITEM.add("straw_bowl", new Item(...), fuel(500));
+ * }</pre>
+ * Example of registration:
+ * <pre>{@code
+ * public static void init() {
+ *     ITEM.register();
+ * }
+ * }</pre>
+ * ~ Jack
+ * @param <T> What type this RegistryQueue registers
+ * @author Solly Watkins
+ */
 @SuppressWarnings("unused")
 public final class RegistryQueue<T> {
     private static final boolean CLIENT = FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
     private final Registry<T> registry;
-    // We only need to append to the end of the list and iterate, so a LinkedList is best.
-    private final LinkedList<Entry<? extends T>> entries;
+    private final Map<Identifier, Entry<? extends T>> entries;
 
+    /**
+     * Creates a {@link RegistryQueue}. This constructor is recommended for mods
+     * that might add blocks regularly and/or don't want to bother dealing with
+     * the other constructor.
+     */
     public RegistryQueue(Registry<T> registry) {
-        this.registry = registry;
-        entries = new LinkedList<>();
+        this(registry, 16); // 16 is the default initial capacity for maps.
     }
 
-    @Deprecated(forRemoval = true, since = "1.7.INDEV3")
+    /**
+     * Creates a {@link RegistryQueue} with an initial capacity for minor speed and memory improvements,
+     * if you're into that sort of thing. Make sure to be diligent when choosing the initial capacity.
+     * @param initialCapacity The initial capacity of this {@link RegistryQueue}'s map. This should
+     *                        ideally be equal to the amount of things you intend to register.
+     */
     public RegistryQueue(Registry<T> registry, int initialCapacity) {
-        this(registry);
+        this.registry = registry;
+        /*
+        A load factor of 0.10F is chosen because people will forget
+        to update this value when they add a few new blocks.
+        The default load factor is 0.75F, which is ridiculous for this use-case.
+         */
+        entries = new LinkedHashMap<>(initialCapacity, 0.10F);
     }
 
+    /**
+     * This method is now deprecated. Instead use {@link Action#onClient()}.
+     * @return {@link Action#onClient()}
+     */
+    @Deprecated(forRemoval = true, since = "1.7-rc.4")
     public static <S> Action<S> onClient(Action<S> action) {
-        return CLIENT ? action : (id, value) -> {};
+        return action.onClient();
     }
 
+    /**
+     * Adds an entry to the {@link RegistryQueue}. If the id is a duplicate, the entry in this RegistryQueue is replaced.
+     * @param id The identifier of the object
+     * @param value The object to be associated with the identifier
+     * @param additionalActions Any additional actions to perform on the object
+     * @param <V> The type or a subtype of this {@link RegistryQueue}'s type T.
+     * @return The {@code value} inputted into this method
+     */
     @SafeVarargs
     public final <V extends T> V add(Identifier id, V value, BiConsumer<Identifier, ? super V>... additionalActions) {
-        this.entries.add(new Entry<>(id, value, additionalActions));
+        this.entries.put(id, new Entry<>(value, additionalActions));
         return value;
     }
 
+    /**
+     * Registers all of the entries in this {@link RegistryQueue} and clears
+     * this RegistryQueue's data from memory. All actions assigned to objects
+     * in this RegistryQueue will be performed at this time.
+     */
     public void register() {
-        for (Entry<? extends T> entry : entries) {
-            register(entry);
-        }
+        entries.forEach(this::register);
         entries.clear();
     }
 
-    private <V extends T> void register(Entry<V> entry) {
-        Registry.register(this.registry, entry.id, entry.value);
-        for (BiConsumer<Identifier, ? super V> action : entry.additionalActions) {
-            action.accept(entry.id, entry.value);
+    private <V extends T> void register(Identifier id, Entry<V> pair) {
+        Registry.register(this.registry, id, pair.value);
+        for (var action : pair.actions) {
+            action.accept(id, pair.value);
         }
     }
 
+    /**
+     * An interface to represent an action to be performed on an object.
+     * Made for ease of use.
+     * @param <T> The type of the object this Action acts upon.
+     */
+    @FunctionalInterface
     public interface Action<T> extends BiConsumer<Identifier, T> {
+        /**
+         * Makes the action only act when the {@link EnvType} is {@link EnvType#CLIENT}.
+         * @return This action, modified to only occur in a client environment.
+         */
+        default Action<T> onClient(){
+            return CLIENT ? this : (id, value) -> {};
+        }
     }
 
-    private static record Entry<V>(Identifier id, V value, BiConsumer<Identifier, ? super V>[] additionalActions) {
+    /**
+     * A record to represent an object and its actions to be performed on it.
+     * @param <V> The type of object
+     */
+    private static record Entry<V>(V value, BiConsumer<Identifier, ? super V>[] actions) {
     }
 }

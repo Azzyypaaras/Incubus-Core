b5ff9e57c3f57ffa2f54e0cf790f1b5a268d3982
Jack Papel <jackpapel@live.com>
2022-04-12 20:26:02 -0700

    Make InventoryBlockEntity an interface
    And change HopperStrategy to be more unanimous.

--- src/main/java/net/id/incubus_core/be/InventoryBlockEntity.java
+++ src/main/java/net/id/incubus_core/be/InventoryBlockEntity.java
@@ -1,101 +1,92 @@
 package net.id.incubus_core.be;
 
 import net.id.incubus_core.util.InventoryWrapper;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.util.collection.DefaultedList;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.function.Predicate;
+
+import static com.google.common.base.Predicates.alwaysFalse;
+import static com.google.common.base.Predicates.alwaysTrue;
+
 /**
- * An abstract block entity that has an inventory, and a strategy for hoppers.
+ * A block entity that has an inventory and a strategy for hoppers.
  * @see HopperStrategy
  */
 @SuppressWarnings("unused")
-public abstract class InventoryBlockEntity extends BlockEntity implements InventoryWrapper, SidedInventory {
-    protected final DefaultedList<ItemStack> inventory;
-    protected HopperStrategy hopperStrategy;
+public interface InventoryBlockEntity extends InventoryWrapper, SidedInventory {
 
-    public InventoryBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state, int invSize, HopperStrategy hopperStrategy) {
-        super(type, pos, state);
-        inventory = DefaultedList.ofSize(invSize, ItemStack.EMPTY);
-        this.hopperStrategy = hopperStrategy;
+    @Override
+    default int[] getAvailableSlots(Direction side) {
+        return new int[this.getItems().size()];
+    }
+
+    @NotNull
+    HopperStrategy getHopperStrategy();
+
+    @Override
+    default boolean canInsert(int slot, ItemStack stack, @Nullable Direction dir) {
+        return this.getHopperStrategy().canInsert(dir);
     }
 
     @Override
-    public int[] getAvailableSlots(Direction side) {
-        return new int[inventory.size()];
-    }
-
-    @Override
-    public DefaultedList<ItemStack> getItems() {
-        return inventory;
-    }
-
-    @Override
-    public boolean canInsert(int slot, ItemStack stack, @Nullable Direction dir) {
-        return switch (hopperStrategy) {
-            case IN_TOP_OUT_BOTTOM -> dir == Direction.UP;
-            case IN_ANY_OUT_BOTTOM -> dir != Direction.DOWN;
-            case IN_ANY, ALL_PASS -> true;
-            default -> false;
-        };
-    }
-
-    @Override
-    public boolean canExtract(int slot, ItemStack stack, Direction dir) {
-        return switch (hopperStrategy) {
-            case IN_TOP_OUT_BOTTOM, IN_ANY_OUT_BOTTOM -> dir == Direction.DOWN;
-            case OUT_ANY, ALL_PASS -> true;
-            default -> false;
-        };
-    }
-
-    @Override
-    public void writeNbt(NbtCompound nbt) {
-        super.writeNbt(nbt);
-        Inventories.writeNbt(nbt, inventory);
-    }
-
-    @Override
-    public void readNbt(NbtCompound nbt) {
-        super.readNbt(nbt);
-        Inventories.readNbt(nbt, inventory);
+    default boolean canExtract(int slot, ItemStack stack, Direction dir) {
+        return this.getHopperStrategy().canExtract(dir);
     }
 
     /**
      * A strategy for how to deal with hoppers and the items that they may want to pass
      */
-    public enum HopperStrategy {
+    @SuppressWarnings("Guava")
+    enum HopperStrategy {
         /**
          * Hopper items can enter through the top and leave through the bottom
          */
-        IN_TOP_OUT_BOTTOM,
+        IN_TOP_OUT_BOTTOM(dir -> dir == Direction.UP, dir -> dir == Direction.DOWN),
         /**
          * Hopper items can enter from any side and leave through the bottom
          */
-        IN_ANY_OUT_BOTTOM,
+        IN_ANY_OUT_BOTTOM(alwaysTrue(), dir -> dir == Direction.DOWN),
         /**
          * Hopper items can enter from any side, but cannot leave
          */
-        IN_ANY,
+        IN_ANY(alwaysTrue(), alwaysFalse()),
         /**
          * Hopper items can leave from any side, but cannot enter
          */
-        OUT_ANY,
+        OUT_ANY(alwaysFalse(), alwaysTrue()),
         /**
          * Hopper items can enter and leave from any side
          */
-        ALL_PASS,
+        ALL_PASS(alwaysTrue(), alwaysTrue()),
         /**
          * Hopper items cannot enter or leave this from any side
          */
-        NO_PASS
+        NO_PASS(alwaysFalse(), alwaysFalse());
+
+        private final Predicate<@Nullable Direction> canInsert;
+        private final Predicate<Direction> canExtract;
+
+        HopperStrategy(Predicate<@Nullable Direction> canInsert, Predicate<Direction> canExtract) {
+            this.canInsert = canInsert;
+            this.canExtract = canExtract;
+        }
+
+        /**
+         * @return Whether this strategy can receive items from the given direction
+         */
+        public final boolean canInsert(@Nullable Direction dir) {
+            return this.canInsert.test(dir);
+        }
+
+        /**
+         * @return Whether this strategy can extract items from the given direction
+         */
+        public final boolean canExtract(Direction dir) {
+            return this.canExtract.test(dir);
+        }
     }
 }

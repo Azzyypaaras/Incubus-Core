372ec927d7e2b4525b35c8cf88217515774ac0fd
Jack Papel <jackpapel@live.com>
2022-05-22 19:29:12 -0700

    Fix condition registration
    You might not believe it, but in order for cardinal components to handle conditions, it needs to know conditions exist

--- src/main/java/net/id/incubus_core/dev/DevInit.java
+++ src/main/java/net/id/incubus_core/dev/DevInit.java
@@ -4,15 +4,27 @@ import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.loader.api.FabricLoader;
 import net.id.incubus_core.IncubusCore;
+import net.id.incubus_core.condition.IncubusCondition;
+import net.id.incubus_core.condition.api.Condition;
+import net.id.incubus_core.condition.api.Severity;
 import net.id.incubus_core.dev.item.EntityDeathMessageTestItem;
 import net.id.incubus_core.woodtypefactory.api.WoodSettingsFactory;
 import net.id.incubus_core.woodtypefactory.api.WoodTypeFactory;
 import net.minecraft.block.Block;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.PillarBlock;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 
 import static net.id.incubus_core.IncubusCore.*;
 import static net.minecraft.item.Items.DIAMOND;
@@ -31,6 +43,22 @@ public final class DevInit {
     
     private static final ItemGroup GROUP = FabricItemGroupBuilder.build(IncubusCore.locate("dev"), () -> new ItemStack(DIAMOND));
     private static final Item ENTITY_DEATH_MESSAGE_ITEM = new EntityDeathMessageTestItem(new FabricItemSettings().group(GROUP));
+    private static final Condition TEST_CONDITION = new Condition(TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier("a")), 100, 100, 5, 5, 1, 50) {
+        @Override
+        public void tick(World world, LivingEntity entity, Severity severity, float rawSeverity) {
+        }
+
+        @Override
+        public void tickPlayer(World world, PlayerEntity player, Severity severity, float rawSeverity) {
+            if (severity.isAsOrMoreSevere(Severity.MILD)) {
+                player.addStatusEffect(new StatusEffectInstance(ZONKED, 10));
+            }
+        }
+
+        @Override
+        public void clientTick(ClientWorld world, LivingEntity entity, Severity severity, float rawSeverity) {
+        }
+    };
 
     private static final WoodTypeFactory INCUBUS_WOOD =
             new WoodTypeFactory(
@@ -50,5 +78,6 @@ public final class DevInit {
 
     public static void clientInit() {
         INCUBUS_WOOD.registerClient();
+        Registry.register(IncubusCondition.CONDITION_REGISTRY, IncubusCore.locate("test_condition"), TEST_CONDITION);
     }
 }

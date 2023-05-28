344c893ace6c758ef9c43fefd14c58641e3e571f
Noaaan <noaaan@hotmail.com>
2023-05-28 03:35:21 +0200

    Make HeatHelper use Conventional Biome Tags

--- src/main/java/net/id/incubus_core/systems/HeatHelper.java
+++ src/main/java/net/id/incubus_core/systems/HeatHelper.java
@@ -1,6 +1,8 @@
 package net.id.incubus_core.systems;
 
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBiomeTags;
 import net.id.incubus_core.util.RandomShim;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
@@ -13,8 +15,8 @@ public class HeatHelper {
         return medium.heatConductivity() * exchangeArea * (tempA - tempB);
     }
 
-    public static double playerAmbientHeat(Material medium, double playerTemp, BlockPos playerPos, Biome biome, boolean night, boolean rain) {
-        return -exchangeHeat(medium, playerTemp, translateBiomeHeat(playerPos, biome, night, rain), 4.5);
+    public static double playerAmbientHeat(Material medium, double playerTemp, BlockPos playerPos, RegistryEntry<Biome> entry, boolean night, boolean rain) {
+        return -exchangeHeat(medium, playerTemp, translateBiomeHeat(playerPos, entry, night, rain), 4.5);
     }
 
     public static double simulateAmbientHeating(HeatIo io, World world, BlockPos pos, Simulation simulation) {
@@ -25,7 +27,7 @@ public class HeatHelper {
             var exchangeDir = io.getPreferredDirection().orElse(validExchangeDirs.get(0));
 
             if (validExchangeDirs.contains(exchangeDir)) {
-                double transfer = exchangeHeat(DefaultMaterials.AIR, io.getTemperature(), translateBiomeHeat(pos, world.getBiome(pos).value(), world.isNight(), world.isRaining()), io.getExchangeArea(exchangeDir));
+                double transfer = exchangeHeat(DefaultMaterials.AIR, io.getTemperature(), translateBiomeHeat(pos, world.getBiome(pos), world.isNight(), world.isRaining()), io.getExchangeArea(exchangeDir));
                 if (simulation == Simulation.ACT) {
                     io.cool(transfer);
                 }
@@ -35,29 +37,18 @@ public class HeatHelper {
         return 0;
     }
 
-    public static double translateBiomeHeat(BlockPos pos, Biome biome, boolean night, boolean rain) {
+    public static double translateBiomeHeat(BlockPos pos, RegistryEntry<Biome> biomeEntry, boolean night, boolean rain) {
 
-        double baseTemp = biome.getTemperature();
-        /*FIXME They remove Biome categories from what I can tell. This is just to make it compile for now.
-        Biome.Category category = ((BiomeAccessor) (Object) biome).getCategory();
-
-        double temp = switch (category) {
-            case NETHER -> baseTemp + 1;
-            case THEEND -> baseTemp - 1.5;
-            case DESERT -> baseTemp - 0.25;
-            case OCEAN -> baseTemp + 0.1;
-            default -> baseTemp;
-        };
-         */
-        double temp = baseTemp;
+        Biome biome = biomeEntry.value();
+        double temp = biome.getTemperature();
 
         temp *= 26.25;
 
         if (night) {
-            if (/*category == Biome.Category.DESERT || category == Biome.Category.MESA*/ false) {
+            if (biomeEntry.isIn(ConventionalBiomeTags.DESERT) || biomeEntry.isIn(ConventionalBiomeTags.MESA)) {
                 temp -= temp * 0.75;
             } else if (biome.getPrecipitation(pos) == Biome.Precipitation.SNOW) {
-                if (/*category == Biome.Category.ICY*/ false && temp < 0)
+                if (biomeEntry.isIn(ConventionalBiomeTags.ICY) && temp < 0)
                     temp += temp * 1.25;
                 else
                     temp -= 20;
